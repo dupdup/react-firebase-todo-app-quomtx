7cf6b991ea3309770f621456020d7dec97dc5686
dupdup <dorukst@gmail.com>
2020-03-12 16:19:11 +0300

    Initial commit

---
+++ Config/config.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyC-x9PJwzT3WbcjZVnozyl6sfdPPBtvv4w",
+    authDomain: "dua-app-482d8.firebaseapp.com",
+    databaseURL: "https://dua-app-482d8.firebaseio.com",
+    projectId: "dua-app-482d8",
+    storageBucket: "dua-app-482d8.appspot.com",
+    messagingSenderId: "1038928291848",
+    appId: "1:1038928291848:web:715c0fef81cdc6765349bd",
+    measurementId: "G-DZX246C711"
+  };
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-firebase-todo-app-quomtx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-firebase-todo-app-quomtx)
